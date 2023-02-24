fda67191517e0dac772c4d8883252c0f7d7944f1
NCOURCHAN001 <ncourchan001@vplincsci047.ad.csbsju.edu>
2023-02-23 19:13:45 -0600

    New updates from Nate

--- src/edu/csbsju/cs/SimplePrint.java
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -22,7 +22,7 @@ public class SimplePrint {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Aidan Thomas and James Strabala");
+		System.out.println("Aidan Thomas and James Strabala are very cool guys");
 		
 		for(int i=0; i<args.length;i++){ 
 		    System.out.println("args[ " + i + " ] = " + args[i] ); 
