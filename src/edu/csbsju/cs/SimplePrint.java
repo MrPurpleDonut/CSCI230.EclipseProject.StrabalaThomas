c0f8ce79a9ffbf58d3a463d8bca80be6239c6e5d
ATHOMAS001 <athomas001@vplincsci032.ad.csbsju.edu>
2023-02-23 18:43:58 -0600

    Commiting

--- src/edu/csbsju/cs/SimplePrint.java
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -3,6 +3,20 @@ package edu.csbsju.cs;
 public class SimplePrint {
 
 	
+	private String pname;
+	
+	private int pAge;
+	
+	
+	/**
+	 * @param pname
+	 * @param pAge
+	 */
+	public SimplePrint(String pname, int pAge) {
+		this.pname = pname;
+		this.pAge = pAge;
+	}
+
 	public SimplePrint() {
 		
 	}
@@ -14,6 +28,34 @@ public class SimplePrint {
 		    System.out.println("args[ " + i + " ] = " + args[i] ); 
 		} 
 
+	}
+
+	/**
+	 * @return the pname
+	 */
+	public String getPname() {
+		return pname;
+	}
+
+	/**
+	 * @param pname the pname to set
+	 */
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	/**
+	 * @return the pAge
+	 */
+	public int getpAge() {
+		return pAge;
+	}
+
+	/**
+	 * @param pAge the pAge to set
+	 */
+	public void setpAge(int pAge) {
+		this.pAge = pAge;
 	}
 
 }
